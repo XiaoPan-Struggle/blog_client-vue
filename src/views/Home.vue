141452388f941d73e7d3a1f98af1a4f82a97ba03
XiaoPan <2489964425@qq.com>
2021-03-30 08:44:53 +0800

    封装 axios 拦截器

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,10 @@
 
 <script>
 // @ is an alias to /src
+import request from '@/helpers/request';
+import auth from '@/api/auth';
+window.request  = request
+window.auth = auth
 
 export default {
   name: 'Home',
