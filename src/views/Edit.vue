96f60c432a25fa612cdfd705cac2209126ec5102
XiaoPan <2489964425@qq.com>
2021-04-11 20:03:41 +0800

    edit.vue 样式完成

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,5 +1,17 @@
 <template>
-<div></div>
+  <div id="edit">
+    <h1>编辑文章</h1>
+    <h3>文章标题</h3>
+    <el-input></el-input>
+    <p class="msg">限30个字</p>
+    <h3>内容简介</h3>
+    <el-input type="textarea" rows=3></el-input>
+    <p class="msg">限30个字</p>
+    <h3>文章内容</h3>
+    <el-input type="textarea" rows=20></el-input>
+    <p class="msg">限30个字</p>
+    <el-button plain type="primary" >确定</el-button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,5 +21,22 @@ name: "Edit"
 </script>
 
 <style lang="scss" scoped>
+#edit, #create {
+  padding: 30px;
 
+  h1 {
+    text-align: center;
+  }
+
+  .msg {
+    text-align: right;
+    font-size: 12px;
+    color: red;
+  }
+
+  ::v-deep .el-input__inner, ::v-deep .el-textarea__inner{
+    border: 1px solid #42b983;
+    background: #ddd;
+  }
+}
 </style>
