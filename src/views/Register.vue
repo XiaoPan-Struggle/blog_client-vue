eefa01e51b6a5cef816ea20167fdcad686d0c4a1
XiaoPan <2489964425@qq.com>
2021-04-11 20:22:06 +0800

    Register.vue 样式完成

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,18 +1,65 @@
 <template>
-  <div>
-    <el-button>注册</el-button>
-    <router-link to="/login">
-      <p>已有账号，去登陆</p>
-    </router-link>
+  <div id="register">
+    <h4>用户名</h4>
+    <el-input v-model="username" placeholder="用户名"></el-input>
+    <p class="error">当前用户名已注册</p>
+    <h4>密码</h4>
+    <el-input v-model="password" type="password" placeholder="密码" ></el-input>
+    <p class="error">当前用户名已注册</p>
+    <el-button size="medium">立即注册</el-button>
+    <p class="notice">已有账号？<router-link to="/login">立即登录</router-link></p>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Register'
+  name: 'Register',
+  data(){
+    return{
+      username: '',
+      password:''
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+#login, #register {
+  display: grid;
+  justify-content: center;
+  padding-top: 30px;
+
+  h4 {
+    margin: 10px 0 5px;
+  }
+
+  p {
+    margin: 5px 0;
+  }
+
+  input {
+    width: 400px;
+  }
+
+  .error {
+    font-size: 12px;
+    color: #f00;
+  }
+
+  button {
+    margin-top: 10px;
+    justify-self: start;
+  }
+
+  .notice {
+    font-size: 12px;
+    text-align: center;
+    margin-top: 30px;
+
+    a {
+      color: #42b983;
+    }
+  }
+}
 
 </style>
