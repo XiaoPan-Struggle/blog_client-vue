9460900db65af67316c0218b116e01d784b79763
XiaoPan <2489964425@qq.com>
2021-04-11 20:19:31 +0800

    Login.vue 样式完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <router-link to="/register">没有账号，去注册</router-link>
-    <el-button>登录</el-button>
+  <div id="login">
+    <h4>用户名</h4>
+    <el-input v-model="username" placeholder="用户名"></el-input>
+    <p class="error">当前用户名已注册</p>
+    <h4>密码</h4>
+    <el-input v-model="password" type="password" placeholder="密码" ></el-input>
+    <p class="error">当前用户名已注册</p>
+    <el-button size="medium">立即登录</el-button>
+    <p class="notice">没有账号？<router-link to="/register">注册新用户</router-link></p>
   </div>
 </template>
 
@@ -12,5 +18,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#login, #register {
+  display: grid;
+  justify-content: center;
+  padding-top: 30px;
 
+  h4 {
+    margin: 10px 0 5px;
+  }
+
+  p {
+    margin: 5px 0;
+  }
+
+  input {
+    width: 400px;
+  }
+
+  .error {
+    font-size: 12px;
+    color: #f00;
+  }
+
+  button {
+    margin-top: 10px;
+    justify-self: start;
+  }
+
+  .notice {
+    font-size: 12px;
+    text-align: center;
+    margin-top: 30px;
+
+    a {
+      color: #42b983;
+    }
+  }
+}
 </style>
