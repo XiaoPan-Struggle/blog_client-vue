b1aa8f9ddbf38ab8313c34ccd56a4f15f29146bc
XiaoPan <2489964425@qq.com>
2021-04-15 20:57:25 +0800

    My.vue 页面完成

--- src/views/My.vue
+++ src/views/My.vue
@@ -1,57 +1,119 @@
 <template>
   <div id="my">
     <section class="user-info">
-      <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="" class="avatar">
-      <h3>若愚</h3>
+      <img :src="user.avatar" :alt="user.username" class="avatar">
+      <h3>{{ user.username }}</h3>
     </section>
     <section>
-      <div class="item">
+      <router-link :to="`/detail/${blog.id}`" class="item" v-for="blog in blogs" :key="blog.id">
         <div class="date">
-          <span class="day">20</span>
-          <span class="month">5月</span>
-          <span class="year">2018</span>
+          <span class="day">{{ splitDate(blog.createdAt).date }}</span>
+          <span class="month">{{ splitDate(blog.createdAt).month }}</span>
+          <span class="year">{{ splitDate(blog.createdAt).year }}</span>
         </div>
-        <h3>前端异步解密</h3>
-        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
+        <h3>{{ blog.title }}</h3>
+        <p>{{ blog.description }}</p>
         <div class="actions">
-          <router-link to="/edit" class="editBtn">编辑</router-link>
-          <a href="#">删除</a>
+          <router-link :to="`/edit/${blog.id}`">编辑</router-link>
+          <a href="#" style="margin-left: 10px" @click.prevent="onDelete(blog.id)">删除</a>
         </div>
-      </div>
-
-      <div class="item">
-        <div class="date">
-          <span class="day">20</span>
-          <span class="month">5月</span>
-          <span class="year">2018</span>
-        </div>
-        <h3>前端异步解密</h3>
-        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
-        <div class="actions">
-          <router-link to="/edit" class="editBtn">编辑</router-link>
-          <a href="#">删除</a>
-        </div>
-      </div>
+      </router-link>
+    </section>
+    <section class="pagination">
+      <el-pagination
+          background
+          @current-change="handleCurrentChange"
+          layout="prev, pager, next"
+          :current-page.sync="page"
+          :page-size="20"
+          :total="total">
+      </el-pagination>
     </section>
   </div>
 </template>
 
-<script lang="ts">
+<script>
+import blog from '@/api/blog.js'
+import {mapGetters} from 'vuex'
+
 export default {
-name: "My"
+  name: 'My',
+  data() {
+    return {
+      blogs: [],
+      page: 1,
+      total: 0
+    }
+  },
+  computed: {
+    ...mapGetters(['user'])
+  },
+  created() {
+    this.page = parseInt(this.$route.params.page) || 1
+    this.getMyBlogInfo()
+  },
+  methods: {
+    getMyBlogInfo(){
+      blog.getBlogByUserId(this.user.id, {page: this.page})
+          .then(res => {
+            console.log(res);
+            let {page, total, data} = res
+            this.page = page
+            this.total = total
+            this.blogs = data
+            if (res.data.length > 0) {
+              this.user = res.data[0].user
+            }
+          })
+    },
+    handleCurrentChange(val) {
+      blog.getBlogByUserId(this.user.id, {page: val})
+          .then(res => {
+            console.log(res);
+            this.blogs = res.data
+            this.total = res.total
+            this.page = res.page
+            this.$router.push({path: '/my', query: {page: val}})
+          })
+    },
+    splitDate(dataStr) {
+      let dateObj = dataStr.split('-')
+      let year = dateObj[0]
+      let month = dateObj[1]
+      let date = dateObj[2].slice(0, 2)
+      return {
+        date,
+        month,
+        year
+      }
+    },
+    async onDelete(blogId) {
+      console.log(blogId);
+      await this.$confirm('是否删除这条博客?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+      await blog.deleteBlog({blogId})
+      this.$message.success('删除成功')
+      // this.getMyBlogInfo()
+      this.blogs = this.blogs.filter(blog => blog.id !== blogId)
+    }
+  }
 }
+
 </script>
 
 <style lang="scss" scoped>
-#my{
+#my {
 
-  .user-info{
+  .user-info {
     display: grid;
     grid: auto auto / 80px 1fr;
     padding: 20px 0;
     border-bottom: 1px solid #aaa;
 
-    .avatar{
+    .avatar {
       grid-column: 1;
       grid-row: 1 / span 2;
       justify-self: center;
@@ -60,17 +122,18 @@ name: "My"
       border-radius: 50%;
     }
 
-    h3{
+    h3 {
       font-size: 16px;
     }
 
   }
-  .item{
+
+  .item {
     display: grid;
     grid: auto auto auto / 80px 1fr;
     margin: 40px 0;
 
-    .date{
+    .date {
       grid-column: 1;
       grid-row: 1 / span 3;
       justify-self: center;
@@ -86,29 +149,36 @@ name: "My"
       }
     }
 
-    h3{
+    h3 {
       grid-column: 2;
       grid-row: 1;
       font-size: 16px;
     }
 
-    p{
+    p {
       grid-column: 2;
       grid-row: 2;
     }
 
-    .actions{
+    .actions {
       grid-column: 2;
       grid-row: 3;
       font-size: 12px;
 
-      a{
+      a {
         color: #009bbb;
-        &.editBtn{
+
+        &.editBtn {
           margin-right: 10px;
         }
       }
     }
+  }
+
+  .pagination {
+    display: grid;
+    justify-items: center;
+    margin-bottom: 20px;
   }
 }
 </style>
